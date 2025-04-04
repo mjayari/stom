76892df238ee0d7f585b44d0bdc0f6f3689e5135
DESKTOP-P9AARU0\mjayari <ayari_med_jamil@hotmail.com>
2025-04-04 19:11:21 +0100

    sixth commit

---
+++ wp-content/plugins/the-events-calendar/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'the-events-calendar/the-events-calendar',
+        'pretty_version' => 'dev-release/T25.antman.1',
+        'version' => 'dev-release/T25.antman.1',
+        'reference' => '7d9941866eac59d3302c275ee99ec113cb9b68ea',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'the-events-calendar/the-events-calendar' => array(
+            'pretty_version' => 'dev-release/T25.antman.1',
+            'version' => 'dev-release/T25.antman.1',
+            'reference' => '7d9941866eac59d3302c275ee99ec113cb9b68ea',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
